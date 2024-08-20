25d5d5693626d4906e9b15c2479ea388a92c3486
Eric Nichols <ebn646@hotmail.com>
2024-08-20 17:20:58 -0400

    first commit

---
+++ api/index.js
@@ -0,0 +1,77 @@
+import express from "express";
+import serverless from "serverless-http";
+import cors from "cors";
+import { fetchTasks, createTasks, updateTasks, deleteTasks } from "./task.js";
+
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+if (process.env.DEVELOPMENT) {
+  // CORS options
+  const corsOptions = {
+    origin: "http://localhost:3000", // Allow only requests from your React app
+    optionsSuccessStatus: 200, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+  };
+
+  // Enable CORS with the specified options
+  app.use(cors(corsOptions));
+}
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/task", async (req, res) => {
+  try {
+    const tasks = await fetchTasks();
+
+    res.send(tasks.Items);
+  } catch (err) {
+    res.status(400).send(`Error fetching tasks: ${err}`);
+  }
+});
+
+app.post("/task", async (req, res) => {
+  try {
+    const task = req.body;
+
+    const response = await createTasks(task);
+
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error creating tasks: ${err}`);
+  }
+});
+
+app.put("/task", async (req, res) => {
+  try {
+    const task = req.body;
+
+    const response = await updateTasks(task);
+
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error updating tasks: ${err}`);
+  }
+});
+
+app.delete("/task/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const response = await deleteTasks(id);
+
+    res.send(response);
+  } catch (err) {
+    res.status(400).send(`Error deleting tasks: ${err}`);
+  }
+});
+if (process.env.DEVELOPMENT) {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
+
+export const handler = serverless(app);
---
+++ ui/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { AddTaskForm } from "./components/AddTaskForm";
+import { Task } from "./components/Task";
+import axios from "axios";
+import { API_URL } from "./utils";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
+export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const fetchTasks = async () => {
+    try {
+      const { data } = await axios.get('https://55pgbae5x5k4zqc3d5sha4nxpa0vlcwj.lambda-url.us-east-2.on.aws/task');
+
+      setTasks(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <AddTaskForm fetchTasks={fetchTasks} />
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} fetchTasks={fetchTasks} />
+      ))}
+    </ThemeProvider>
+  );
+}
